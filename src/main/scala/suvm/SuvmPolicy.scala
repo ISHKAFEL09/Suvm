55cfc0cc27ecb73f4dc4f3cebe5ba792f92186e4
ISHKAFEL09 <wyin09@qq.com>
2023-05-29 11:07:37 +0800

    Delete src/main/scala/suvm directory

--- src/main/scala/suvm/SuvmPolicy.scala
+++
@@ -1,7 +0,0 @@
-package suvm
-
-abstract class SuvmPolicy extends SuvmObject {
-  def getActiveObjectDepth: Boolean = false
-
-  def flush(): Unit = {}
-}
--- src/main/scala/suvm/SuvmTrStream.scala
+++
@@ -1,9 +0,0 @@
-package suvm
-
-import SuvmImplicits._
-
-class SuvmTrStream(val name: String) extends SuvmObject {
-  def getDb: SuvmTrDatabase = ???
-
-  def openRecorder(name: String, openTime: Time = 0.s, typeName: String = ""): Option[SuvmRecorder] = ???
-}
